eff832e70b8b5a1343a980e9ea589e9aa4d78345
lemaiwo <wouterlemaire120@hotmail.com>
2020-10-01 19:43:59 +0000

    select camera + change mirror setting

--- ScannerApp/webapp/control/QRScanner.js
+++ ScannerApp/webapp/control/QRScanner.js
@@ -1,43 +1,65 @@
-sap.ui.define(["sap/ui/core/Control"], function (Control) {
-	"use strict";
-	return Control.extend("be.wl.ScannerApp.control.QRScanner", {
-		"metadata": {
-			"properties": {
-				value:'string'
-			},
-			"events": {
-				QRCodeScanned: {
-					parameters: {
-						value: {
-							type: "string"
-						}
-					}
-				}
-			}
-		},
-		init: function () { },
-		onAfterRendering: function (evt) {
-			let scanner = new Instascan.Scanner({
-				video: this.getDomRef()
-			});
-			scanner.addListener('scan', (qrcode) => this.onQRCodeScanned(qrcode));
-			Instascan.Camera.getCameras().then(function (cameras) {
-				if (cameras.length > 0) {
-					scanner.start(cameras[0]);
-				} else {
-					console.error('No cameras found.');
-				}
-			}).catch(function (e) {
-				console.error(e);
-			});
-		},
-		onQRCodeScanned: function (qrcode) {
-			// this.setValue(qrcode);
-			//use setproperty with suppressinvalidate true to avoid rerender of control
-			this.setProperty("value",qrcode,true);
-			this.fireQRCodeScanned({
-				value: qrcode
-			});
-		}
-	});
+sap.ui.define(["sap/ui/core/Control", "sap/m/Select", "sap/ui/core/Item","sap/m/Switch","sap/m/Toolbar"], function (Control, Select, Item,Switch,Toolbar) {
+    "use strict";
+    return Control.extend("be.wl.ScannerApp.control.QRScanner", {
+        "metadata": {
+            "properties": {
+                value: 'string'
+            },
+            "events": {
+                QRCodeScanned: {
+                    parameters: {
+                        value: {
+                            type: "string"
+                        }
+                    }
+                }
+            },
+            aggregations: {
+                _toolbar: {
+                    type: "sap.m.Toolbar",
+                    multiple: false,
+                    visibility: "hidden"
+                }
+            }
+        },
+        init: function () {
+            this._cameraSelection = new Select({change:this.onChangeCamera.bind(this)});
+            this._mirrorSwitch = new Switch({state:false,change:this.onChangeMirror.bind(this)});
+            this.setAggregation("_toolbar", new Toolbar({content:[this._cameraSelection,this._mirrorSwitch]}));
+        },
+        onAfterRendering: function (evt) {
+            this.scanner = new Instascan.Scanner({
+                video: this.getDomRef().lastElementChild,
+                mirror:false
+            });
+            this.scanner.addListener('scan', (qrcode) => this.onQRCodeScanned(qrcode));
+            Instascan.Camera.getCameras().then(function (cameras) {
+                this.cameras=cameras;
+                cameras.forEach(function (camera,key) {
+                    this._cameraSelection.addItem(new Item({ key: key, text: camera.name }));
+                }.bind(this))
+                if (cameras.length > 0) {
+                    this.scanner.start(cameras[0]);
+                } else {
+                    console.error('No cameras found.');
+                }
+            }.bind(this)).catch(function (e) {
+                console.error(e);
+            });
+        },
+        onQRCodeScanned: function (qrcode) {
+            // this.setValue(qrcode);
+            //use setproperty with suppressinvalidate true to avoid rerender of control
+            this.setProperty("value", qrcode, true);
+            this.fireQRCodeScanned({
+                value: qrcode
+            });
+        },
+        onChangeCamera:function(event){
+            this.scanner.start(this.cameras[event.getParameter("selectedItem").getKey()]);
+        },
+        onChangeMirror:function(event){
+            this.scanner.mirror = event.getParameter("state");
+        }
+    });
 });
