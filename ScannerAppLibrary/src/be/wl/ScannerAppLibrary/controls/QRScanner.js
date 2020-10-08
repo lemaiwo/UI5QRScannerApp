ed7f56111db6edf7a431f3c3e95f27c1e438df5b
lemaiwo <wouterlemaire120@hotmail.com>
2020-10-08 05:49:54 +0000

    config changes

--- ScannerAppLibrary/src/be/wl/ScannerAppLibrary/controls/QRScanner.js
+++ ScannerAppLibrary/src/be/wl/ScannerAppLibrary/controls/QRScanner.js
@@ -27,41 +27,39 @@ sap.ui.define(["sap/ui/core/Control", "sap/m/Select", "sap/ui/core/Item", "sap/m
             this._mirrorSwitch = new Switch({ state: false, change: this.onChangeMirror.bind(this) });
             this.setAggregation("_toolbar", new Toolbar({ content: [this._cameraSelection, this._mirrorSwitch] }));
         },
-        onAfterRendering: function (evt) {
-            this.scanner = new Instascan.Scanner({
-                video: this.getDomRef().lastElementChild,
-                mirror: false
-            });
-            this.scanner.addListener('scan', function(qrcode){ 
-                this.onQRCodeScanned(qrcode);
-            }.bind(this));
-            Instascan.Camera.getCameras().then(function (cameras) {
-                this.cameras = cameras;
-                cameras.forEach(function (camera, key) {
-                    this._cameraSelection.addItem(new Item({ key: key, text: camera.name }));
-                }.bind(this))
-                if (cameras.length > 0) {
-                    this.scanner.start(cameras[0]);
-                } else {
-                    console.error('No cameras found.');
-                }
-            }.bind(this)).catch(function (e) {
-                console.error(e);
-            });
-        },
-        onQRCodeScanned: function (qrcode) {
-            // this.setValue(qrcode);
-            //use setproperty with suppressinvalidate true to avoid rerender of control
-            this.setProperty("value", qrcode, true);
-            this.fireQRCodeScanned({
-                value: qrcode
-            });
-        },
-        onChangeCamera: function (event) {
-            this.scanner.start(this.cameras[event.getParameter("selectedItem").getKey()]);
-        },
-        onChangeMirror: function (event) {
-            this.scanner.mirror = event.getParameter("state");
+onAfterRendering: function (evt) {
+    this.scanner = new Instascan.Scanner({
+        video: this.getDomRef().lastElementChild,
+        mirror: false
+    });
+    this.scanner.addListener('scan', function(qrcode){ 
+        this.onQRCodeScanned(qrcode);
+    }.bind(this));
+    Instascan.Camera.getCameras().then(function (cameras) {
+        this.cameras = cameras;
+        cameras.forEach(function (camera, key) {
+            this._cameraSelection.addItem(new Item({ key: key, text: camera.name }));
+        }.bind(this))
+        if (cameras.length > 0) {
+            this.scanner.start(cameras[0]);
+        } else {
+            console.error('No cameras found.');
         }
+    }.bind(this)).catch(function (e) {
+        console.error(e);
+    });
+},
+onQRCodeScanned: function (qrcode) {
+    this.setProperty("value", qrcode, true);
+    this.fireQRCodeScanned({
+        value: qrcode
+    });
+},
+onChangeCamera: function (event) {
+    this.scanner.start(this.cameras[event.getParameter("selectedItem").getKey()]);
+},
+onChangeMirror: function (event) {
+    this.scanner.mirror = event.getParameter("state");
+}
     });
 });
--- ScannerAppLibrary/src/be/wl/ScannerAppLibrary/controls/QRScannerRenderer.js
+++ ScannerAppLibrary/src/be/wl/ScannerAppLibrary/controls/QRScannerRenderer.js
@@ -1,13 +1,13 @@
 sap.ui.define([], function () {
     "use strict";
-    var QRScannerRenderer = {
-        apiVersion: 2, 
-        render: function (oRm, oControl) {
-            oRm.openStart("div", oControl).style("width", "100%").style("height", "100%").openEnd();
-            oRm.renderControl(oControl.getAggregation("_toolbar"));
-            oRm.openStart("video", oControl).style("width", "100%").style("height", "100%").openEnd().close("video");
-            oRm.close("div");
-        }
-    };
+var QRScannerRenderer = {
+    apiVersion: 2, 
+    render: function (oRm, oControl) {
+        oRm.openStart("div", oControl).style("width", "100%").style("height", "100%").openEnd();
+        oRm.renderControl(oControl.getAggregation("_toolbar"));
+        oRm.openStart("video", oControl).style("width", "100%").style("height", "100%").openEnd().close("video");
+        oRm.close("div");
+    }
+};
     return QRScannerRenderer;
 }, true);
