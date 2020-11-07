fc1ca7d2a4b1b1a06b8c79b4aeb118d1cb689d77
lemaiwo <wouterlemaire120@hotmail.com>
2020-11-07 21:09:29 +0000

    fix

--- ScannerApp/webapp/controller/App.controller.js
+++ ScannerApp/webapp/controller/App.controller.js
@@ -1,23 +1,73 @@
 sap.ui.define([
-        "sap/ui/core/mvc/Controller",
-        "sap/ui/model/json/JSONModel",
-        "sap/m/MessageToast"
-	],
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast",
+    "sap/m/Button",
+    "sap/m/Dialog",
+    "be/wl/ScannerAppLibrary/controls/QRScanner"
+],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-	function (Controller,JSONModel,MessageToast) {
-		"use strict";
+    function (Controller, JSONModel, MessageToast,Button,Dialog,QRScanner) {
+        "use strict";
 
-		return Controller.extend("be.wl.ScannerApp.controller.App", {
-			onInit: function () {
-                this.getView().setModel(new JSONModel({QRCode:""}),"ui")
+        return Controller.extend("be.wl.ScannerApp.controller.App", {
+            onInit: function () {
+                this.getView().setModel(new JSONModel({ QRCode: "" }), "ui")
             },
-            onQRCodeScanned:function(event){
+            onQRCodeScanned: function (event) {
                 //qr code from event
                 console.log(event.getParameter("value"));
                 //or use binding
-                MessageToast.show("Scanned QRCode: "+ this.getView().getModel("ui").getProperty("/QRCode"));
+                MessageToast.show("Scanned QRCode: " + this.getView().getModel("ui").getProperty("/QRCode"));
+            },
+            onOpenScanner: function () {
+                if (!this.oDefaultDialog) {
+                    this.oDefaultDialog = new Dialog({
+                        title: "Scanner in dialog",
+                        content: new QRScanner({
+                            QRCodeScanned:this.onQRCodeScanned,
+                            value:"{ui>/QRCode}"
+                        }),
+                        endButton: new Button({
+                            text: "Close",
+                            press: function () {
+                                this.oDefaultDialog.getDomRef().style.visibility = "hidden";
+                                //this.oDefaultDialog.getDomRef().classList.remove("sapMDialogOpen");
+                                document.getElementById("sap-ui-blocklayer-popup").style.visibility = "hidden";
+                                //this.oDefaultDialog.close();
+                            }.bind(this)
+                        })
+                    });
+                    // to get access to the controller's model
+                    this.getView().addDependent(this.oDefaultDialog);
+                    this.oDefaultDialog.open();
+                }
+                document.getElementById("sap-ui-blocklayer-popup").style.visibility = "visible";
+                this.oDefaultDialog.getDomRef().style.visibility = "visible";
+                //this.oDefaultDialog.getDomRef().classList.add("sapMDialogOpen");
+                //this.oDefaultDialog.open();
+            },
+            onOpenScanner2: function () {
+                if (!this.oDefaultDialog2) {
+                    this.oDefaultDialog2 = new Dialog({
+                        title: "Scanner in dialog",
+                        content: new QRScanner({
+                            QRCodeScanned:".onQRCodeScanned",
+                            value:"{ui>/QRCode}"
+                        }),
+                        endButton: new Button({
+                            text: "Close",
+                            press: function () {
+                                this.oDefaultDialog.close();
+                            }.bind(this)
+                        })
+                    });
+                    // to get access to the controller's model
+                    this.getView().addDependent(this.oDefaultDialog2);
+                }
+                this.oDefaultDialog2.open();
             }
-		});
-	});
+        });
+    });
--- ScannerAppLibrary/src/be/wl/ScannerAppLibrary/controls/QRScanner.js
+++ ScannerAppLibrary/src/be/wl/ScannerAppLibrary/controls/QRScanner.js
@@ -1,4 +1,4 @@
-sap.ui.define(["sap/ui/core/Control", "sap/m/Select", "sap/ui/core/Item", "sap/m/Switch", "sap/m/Toolbar","../libs/instascan.min","../libs/adapter"], function (Control, Select, Item, Switch, Toolbar,instaLibrary,adapterLibrary) {
+sap.ui.define(["sap/ui/core/Control", "sap/m/Select", "sap/ui/core/Item", "sap/m/Switch", "sap/m/Toolbar", "../libs/instascan.min", "../libs/adapter"], function (Control, Select, Item, Switch, Toolbar, instaLibrary, adapterLibrary) {
     "use strict";
     return Control.extend("be.wl.ScannerAppLibrary.controls.QRScanner", {
         "metadata": {
@@ -27,6 +27,7 @@ sap.ui.define(["sap/ui/core/Control", "sap/m/Select", "sap/ui/core/Item", "sap/m
             this._mirrorSwitch = new Switch({ state: false, change: this.onChangeMirror.bind(this) });
             this.setAggregation("_toolbar", new Toolbar({ content: [this._cameraSelection, this._mirrorSwitch] }));
         },
+<<<<<<< Updated upstream
 onAfterRendering: function (evt) {
     this.scanner = new Instascan.Scanner({
         video: this.getDomRef().lastElementChild,
@@ -44,6 +45,45 @@ onAfterRendering: function (evt) {
             this.scanner.start(cameras[0]);
         } else {
             console.error('No cameras found.');
+=======
+        onAfterRendering: function (evt) {
+            if (!this.scanner) {
+                this.scanner = new Instascan.Scanner({
+                    video: this.getDomRef().lastElementChild,
+                    mirror: false
+                });
+                this.scanner.addListener('scan', function (qrcode) {
+                    this.onQRCodeScanned(qrcode);
+                }.bind(this));
+            }
+            Instascan.Camera.getCameras().then(function (cameras) {
+                this.cameras = cameras;
+                cameras.forEach(function (camera, key) {
+                    this._cameraSelection.addItem(new Item({ key: key, text: camera.name }));
+                }.bind(this))
+                if (cameras.length > 0) {
+                    this.scanner.start(cameras[0]);
+                } else {
+                    console.error('No cameras found.');
+                }
+            }.bind(this)).catch(function (e) {
+                console.error(e);
+            });
+        },
+        onQRCodeScanned: function (qrcode) {
+            // this.setValue(qrcode);
+            //use setproperty with suppressinvalidate true to avoid rerender of control
+            this.setProperty("value", qrcode, true);
+            this.fireQRCodeScanned({
+                value: qrcode
+            });
+        },
+        onChangeCamera: function (event) {
+            this.scanner.start(this.cameras[event.getParameter("selectedItem").getKey()]);
+        },
+        onChangeMirror: function (event) {
+            this.scanner.mirror = event.getParameter("state");
+>>>>>>> Stashed changes
         }
     }.bind(this)).catch(function (e) {
         console.error(e);
